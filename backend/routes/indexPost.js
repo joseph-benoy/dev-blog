7c9c448f862a661b7102f641f36694ffce53692a
Joseph Benoy <josephbenoy03@gmail.com>
2021-06-14 15:37:24 +0530

    changed indexPost

--- backend/routes/indexPost.js
+++ backend/routes/indexPost.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let db = require('../config/db');
 router.get('/',(req,res)=>{
-    db.query(`SELECT ID,TITLE,TITLE_SLAG,DATE,COVER_IMAGE_LOCATION FROM POST`,(err,result,fileds)=>{
+    db.query(`SELECT ID,TITLE,TITLE_SLAG,DATE,COVER_IMAGE_LOCATION,DESCRIPTION FROM POST`,(err,result,fileds)=>{
         if(err){
             res.json(err);
         }
